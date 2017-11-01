53ff18f5459e5df9a3a8692089f00e1a78345907
Phearun Rath <rathphearun168@gmail.com>
2017-11-01 09:05:28 +0700

    change jar to war

--- src/main/java/com/kshrd/LostAndFoundApplication.java
+++ src/main/java/com/kshrd/LostAndFoundApplication.java
@@ -2,6 +2,8 @@ package com.kshrd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
@@ -11,9 +13,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Import({ springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class })
 @SpringBootApplication
 @ComponentScan({"com.kshrd", "com.phearun.fileupload"})
-public class LostAndFoundApplication {
+public class LostAndFoundApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LostAndFoundApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LostAndFoundApplication.class);
 	}
 }
